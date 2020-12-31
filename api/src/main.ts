78e6faea1323e82e5696e7633161bfad569a6af7
cloakdkilla <getrektwithme1@gmail.com>
2020-12-31 00:15:44 +0000

    change port

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3001);
+  await app.listen(3002);
 }
 bootstrap();
