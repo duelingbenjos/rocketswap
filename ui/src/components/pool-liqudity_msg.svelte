19475267be200182238a6ef9fe4cff63baea553e
jeffwscott <jeffscott@outlook.com>
2021-01-04 17:28:38 -0500

    spelling

--- ui/src/components/pool-liqudity_msg.svelte
+++ ui/src/components/pool-liqudity_msg.svelte
@@ -22,7 +22,7 @@
 <div>
     <p><strong>Liquidity provider rewards</strong></p>
     <p>
-        Liqidity providers earn a 0.3% fee on all trades proportional to the their share of the pool. 
-        Fees are added to the pool, accrue in real time and can be claimed by withdrawing your liqidity.
+        Liquidity providers earn a 0.3% fee on all trades proportional to the their share of the pool. 
+        Fees are added to the pool, accrue in real time and can be claimed by withdrawing your liquidity.
     </p>
 </div>
