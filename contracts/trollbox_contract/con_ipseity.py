4b500516fdcb0ed403c8cb5bf45341bc1c73be4d
cloakdkilla <getrektwithme1@gmail.com>
2021-02-03 01:06:26 +0000

    name contract

--- contracts/trollbox_contract/con_ipseity.py
+++ contracts/trollbox_contract/con_ipseity.py
@@ -1,8 +1,8 @@
 # import currency
 
-name_to_key = Hash(default_value='')
-key_to_name = Hash(default_value='')
-auth_codes = Hash(default_value='')
+name_to_key = Hash(default_value=False)
+key_to_name = Hash(default_value=False)
+auth_codes = Hash(default_value=False)
 Owner = Variable()
 
 @construct
@@ -18,9 +18,9 @@ def setName(name: str):
     previous_name = key_to_name[key]
     # 2 ) Assert new name is available
     name_taken = name_to_key[name]
-    assert name_taken is '', 'This name has been taken.'
+    assert name_taken is False, 'This name has been taken.'
     # 3 ) Unset previous name
-    if previous_name is not '':
+    if previous_name is not False:
         name_to_key[previous_name] = ''
     # 4 ) Set new name, using key_to_name and name_to_key
     key_to_name[key] = name
