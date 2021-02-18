717d71845761400b88a8b237c8c0674ba09a0ff2
cloakdkilla <getrektwithme1@gmail.com>
2021-02-18 00:17:55 +0000

    cleanup

--- api/src/authentication/trollbox.service.ts
+++ api/src/authentication/trollbox.service.ts
@@ -1,11 +1,5 @@
-import { Injectable, UnprocessableEntityException } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
-import { SignOptions, TokenExpiredError } from "jsonwebtoken";
-
+import { Injectable } from "@nestjs/common";
 import { NameEntity } from "../entities/name.entity";
-import { RefreshTokensRepository } from "./refresh-token.repository";
-
-import { RefreshTokenEntity } from "../entities/refresh-token.entity";
 import { IKvp } from "../types/misc.types";
 import { config } from "../config";
 import { TokensService } from "./tokens.service";
