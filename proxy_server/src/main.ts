21a3e7b2b6f354d093b312ac0b75d48fe346427c
cloakdkilla <getrektwithme1@gmail.com>
2021-03-16 13:22:41 +0000

    cors D:

--- proxy_server/src/main.ts
+++ proxy_server/src/main.ts
@@ -19,6 +19,25 @@ const API_URL = "http://0.0.0.0:2053";
 app.use(morgan("dev"));
 app.use(cors());
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://rocketswap.exchange');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    // Pass to next layer of middleware
+    next();
+});
+
 function createProxyRoute(prefix, target) {
 	return httpProxy(target, {
 		proxyReqPathResolver: (req) => {
