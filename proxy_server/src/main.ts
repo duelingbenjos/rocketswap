d960d53fc08b58f75f9dcc42722f2124151a6f61
cloakdkilla <getrektwithme1@gmail.com>
2021-03-16 13:18:22 +0000

    cors on proxy server

--- proxy_server/src/main.ts
+++ proxy_server/src/main.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import morgan from "morgan";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import cors from "cors"
 // let httpProxy = require("express-http-proxy");
 import httpProxy from "express-http-proxy";
 
 // Create Express Server
 const app = express();
+
 
 // Configuration
 const PORT = 80;
@@ -15,6 +17,7 @@ const DOCS_URL = "http://0.0.0.0:3000";
 const API_URL = "http://0.0.0.0:2053";
 
 app.use(morgan("dev"));
+app.use(cors());
 
 function createProxyRoute(prefix, target) {
 	return httpProxy(target, {
