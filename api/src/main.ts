feebf1bc1a6f7948fdaa56f8d2f03379eca88b46
cloakdkilla <getrektwithme1@gmail.com>
2021-03-16 13:25:09 +0000

    cors cmon

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({origin: 'https://rocketswap.exchange'});
   await app.listen(2053);
 }
 bootstrap();
--- proxy_server/src/main.ts
+++ proxy_server/src/main.ts
@@ -17,26 +17,27 @@ const DOCS_URL = "http://0.0.0.0:3000";
 const API_URL = "http://0.0.0.0:2053";
 
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors({origin: 'http://rocketswap.exchange'}));
+app.use(cors({origin: 'https://rocketswap.exchange'}));
 
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://rocketswap.exchange');
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'https://rocketswap.exchange');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 function createProxyRoute(prefix, target) {
 	return httpProxy(target, {
