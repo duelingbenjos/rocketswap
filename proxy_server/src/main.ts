5212686c60b4c3c823ec7c120b9b70fcde60d117
cloakdkilla <getrektwithme1@gmail.com>
2021-03-16 10:33:34 +0000

    proxy config

--- proxy_server/src/main.ts
+++ proxy_server/src/main.ts
@@ -7,9 +7,10 @@ const app = express();
 
 // Configuration
 const PORT = 80;
-const WEBSITE_URL = "http://0.0.0.0:82"
-const APP_URL = "http://0.0.0.0:5000"
-const DOCS_URL = "http://0.0.0.0:3000"
+const WEBSITE_URL = "http://0.0.0.0:82";
+const APP_URL = "http://0.0.0.0:5000";
+const DOCS_URL = "http://0.0.0.0:3000";
+const API_URL = "http://0.0.0.0:2053";
 
 app.use(morgan("dev"));
 
@@ -36,21 +37,29 @@ app.use(
 	})
 );
 
+// app.use(
+// 	"/docs",
+// 	createProxyMiddleware({
+// 		target: DOCS_URL,
+// 		changeOrigin: true,
+// 		pathRewrite: {
+// 			[`^/docs`]: ""
+// 		}
+// 	})
+// );
+
 app.use(
-	"/docs",
+	"/cxn",
 	createProxyMiddleware({
-		target: DOCS_URL,
+		target: API_URL,
 		changeOrigin: true,
 		pathRewrite: {
-			[`^/docs`]: ""
+			[`^/cxn`]: ""
 		}
 	})
 );
 
-
-
 // Start the Proxy
 app.listen(PORT, () => {
-    console.log(`Starting Proxy on port : ${PORT}`);
- });
- 
+	console.log(`Starting Proxy on port : ${PORT}`);
+});
