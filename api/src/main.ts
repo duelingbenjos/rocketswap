12a35a766766b31389b856176e4f4f432d4d33d5
cloakdkilla <getrektwithme1@gmail.com>
2021-03-23 01:45:32 +0000

    cors fix

--- api/src/main.ts
+++ api/src/main.ts
@@ -14,6 +14,14 @@ if (process.env.CONTEXT === 'remote') {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, options);
+
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "rocketswap.exchange"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "rswp.io"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
   app.enableCors({origin: '*'});
   await app.listen(2053);
 }
