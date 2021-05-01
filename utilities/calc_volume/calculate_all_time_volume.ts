0e6b8abb5ee3c4c40ba99aa9e85b58b525c95423
cloakdkilla <getrektwithme1@gmail.com>
2021-05-01 03:39:12 +0100

    calc tool finished

--- utilities/calc_volume/calculate_all_time_volume.ts
+++ utilities/calc_volume/calculate_all_time_volume.ts
@@ -6,7 +6,9 @@ async function main() {
 		let volume = 0;
 		for (let market of market_summaries_res.data) {
 			let trades = await getTrades(market.contract_name);
-			trades.forEach((trade) => (volume += trade.amount * trade.price));
+			trades.forEach((trade: any) => {
+				volume += parseFloat(trade.amount) * parseFloat(trade.price);
+			});
 		}
 		console.log({ volume });
 	} catch (err) {
@@ -18,7 +20,7 @@ async function getTrades(contract_name: string, skip: number = 0, results: any[]
 	const res: any = await axios.get(
 		`https://rocketswap.exchange:2053/api/get_trade_history?contract_name=${contract_name}&skip=${skip}&take=50`
 	);
-	results.push(res.data);
+	results.push(...res.data);
 	if (res.data.length === 50) {
 		return await getTrades(contract_name, skip + 50, results);
 	} else {
