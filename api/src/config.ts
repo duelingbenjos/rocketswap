632033c7522fc5ba887992d315e2a6f6e44ab897
cloakdkilla <getrektwithme1@gmail.com>
2021-11-03 23:42:11 +0000

    updated config to mainnet

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,11 +1,11 @@
 export const config = {
 	appName: "Rocketswap",
-	contractName: process.env.CONTRACT_NAME || "con_amm_v9",
-	identityContract: process.env.IDENTITY_CONTRACT || "con_ipseity_5",
+	contractName: process.env.CONTRACT_NAME || "con_rocketswap_official_v1_1",
+	identityContract: process.env.IDENTITY_CONTRACT || "con_rocket_id_v1",
 	buy: 150,
-	currencySymbol: process.env.CURRENCY_SYMBOL || "dTau",
-	blockExplorer: process.env.BLOCK_EXPLORER_URL || "https://testnet.lamden.io/api",
-	masternode: process.env.MASTERNODE_URL || "https://testnet-master-1.lamden.io",
-	networkType: process.env.NETWORK_TYPE || "testnet", // or 'mainnet'
+	currencySymbol: process.env.CURRENCY_SYMBOL || "TAU",
+	blockExplorer: process.env.BLOCK_EXPLORER_URL || "https://mainnet.lamden.io/api",
+	masternode: process.env.MASTERNODE_URL || "https://masternode-01.lamden.io",
+	networkType: process.env.NETWORK_TYPE || "mainnet", // or 'mainnet'
 	staking_contract_submittor: "7c296eb80e379171f694a3c5be7640d16f300f09d731c99ac0a92f49c9c0c151"
 };
