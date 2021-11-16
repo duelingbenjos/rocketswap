866bbf32e9c865c312e2cee45f0bfb2c896de9b2
cloakdkilla <getrektwithme1@gmail.com>
2021-11-16 00:27:52 +0000

    buys are buys now

--- api/src/entities/trade-history.entity.ts
+++ api/src/entities/trade-history.entity.ts
@@ -110,7 +110,7 @@ export const parseTrades = async (history: any[], contract_name: string, token_s
 			const curr_reserves_token = getNumberFromFixed(
 				curr_value.state_changes_obj[`${config.amm_contract}`].reserves[`${contract_name}`][1]
 			);
-			const action = prev_reserves_token > curr_reserves_token ? "sell" : "buy";
+			const action = prev_reserves_token < curr_reserves_token ? "sell" : "buy";
 			const base_volume = action === "buy" ? curr_reserves_token - prev_reserves_token : prev_reserves_token - curr_reserves_token;
 			const tx_uid = curr_value.tx_uid;
 			const timestamp = curr_value.timestamp / 1000;
