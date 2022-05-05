afa9267323b37b41899f41dc61103f0953ea3fcd
cloakdkilla <getrektwithme1@gmail.com>
2022-05-05 12:25:20 +0100

    config updates

--- api/src/config.ts
+++ api/src/config.ts
@@ -22,7 +22,6 @@ export const config_prod: IApiConfig = {
 	masternode: process.env.MASTERNODE_URL || "https://masternode-01.lamden.io",
 	network_type: process.env.NETWORK_TYPE || "mainnet",
 	staking_contract_submittor: "7c296eb80e379171f694a3c5be7640d16f300f09d731c99ac0a92f49c9c0c151",
-	//block_service_urls: process.env.block_service_urls?.split(",") || ["78.141.225.103:3535"]
 	block_service_urls: process.env.block_service_urls?.split(",") || ["165.22.47.195:3535"]
 };
 
